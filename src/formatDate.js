ebfe06923e4e8b93c475f4717489f57b62fc8344
WalentynaSadowa <walentynasadowa28@gmail.com>
2025-02-17 20:12:14 +0100

    advanced

--- src/formatDate.js
+++ src/formatDate.js
@@ -8,7 +8,42 @@
  * @returns {string}
  */
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  const separator = fromFormat[fromFormat.length - 1];
+  const dateParts = date.split(separator);
+
+  const dateMap = {};
+
+  fromFormat.forEach((part, index) => {
+    dateMap[part] = dateParts[index];
+  });
+
+  const resultParts = toFormat.map((format) => {
+    if (format === 'YYYY') {
+      const yy = dateMap['YY'];
+
+      if (yy) {
+        return (parseInt(yy) < 30 ? '20' : '19') + yy;
+      }
+
+      return dateMap['YYYY'];
+    }
+
+    if (format === 'YY') {
+      const yyyy = dateMap['YYYY'];
+
+      if (yyyy) {
+        return yyyy.slice(-2);
+      }
+
+      return dateMap['YY'];
+    }
+
+    return dateMap[format];
+  });
+
+  const formattedDate = resultParts.join(toFormat[toFormat.length - 1]);
+
+  return formattedDate.replace(/([^\d\s])$/, '');
 }
 
 module.exports = formatDate;
